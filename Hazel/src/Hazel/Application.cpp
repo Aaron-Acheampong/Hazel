4db18d1273fdce8b3bf365bdea6a74835e197d74
Aaron-Acheampong <aronzy.as@gmail.com>
2025-12-28 03:19:55 +0000

    Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,20 @@
+#include "Hazel/Application.h"
+
+namespace Hazel
+{
+	Application::Application()
+	{
+
+	}
+
+
+	Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+}
